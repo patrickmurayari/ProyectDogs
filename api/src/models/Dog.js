007bd5125927fdb1537a2b72430ace705cc31326
patrickmurayari <patrickmurayari03@gmail.com>
2023-04-25 08:13:23 -0300

    first commit

---
+++ api/src/models/Dog.js
@@ -0,0 +1,74 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dog', {
+    Id : {
+      type: DataTypes.INTEGER,
+      primaryKey : true,
+      allowNull : false,
+    },
+    Imagen: {
+      type: DataTypes.STRING,
+      validate : {
+        isUrl : true,
+      },
+      allowNull : false,
+    },
+    Name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Altura : {
+      type:  DataTypes.STRING,
+      allowNull: false,
+    },
+    Peso : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lifes_span : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },{ timestamps: false });
+};
+
+
+/* 
+
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dog', {
+    id : {
+      type: DataTypes.INTEGER,
+      primaryKey : true,
+      allowNull : false,
+    },
+    image : {
+      type: DataTypes.STRING,
+      validate : {
+        isUrl : true,
+      },
+      allowNull : false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight : {
+      type:  DataTypes.STRING,
+      allowNull: false,
+    },
+    height : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    life_span : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  });
+};
+*/
